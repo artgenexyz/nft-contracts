709d8c09470cb5c0dde3f03b90ffd0122010c4e9
Aleksey Bykhun <theisease@gmail.com>
2021-09-11 03:55:47 +0800

    make AvatarNFT reference implementation, use TemplateNFT everywhere

--- test/template_nft.js
+++ test/template_nft.js
@@ -1,27 +1,27 @@
-const AvatarNFT = artifacts.require("AvatarNFT");
+const TemplateNFT = artifacts.require("TemplateNFT");
 
 const ether = 1e18;
 
-contract("AvatarNFT", accounts => {
+contract("TemplateNFT", accounts => {
     // it should deploy successfully
     it("should deploy successfully", async () => {
-        const nft = await AvatarNFT.deployed();
+        const nft = await TemplateNFT.deployed();
         assert.ok(nft.address);
     });
 
-    // price should equal 0.03 ether
-    it("should have a price of 0.03 ether", async () => {
-        const nft = await AvatarNFT.deployed();
+    // price should equal 1 ether
+    it("should have a price of 1 ether", async () => {
+        const nft = await TemplateNFT.deployed();
         const price = await nft.getPrice();
-        assert.equal(price, (0.03 * ether).toString());
+        assert.equal(price, (1 * ether).toString());
     });
 
     // it should fail to mint when sale is not started
     it("should fail to mint when sale is not started", async () => {
-        const nft = await AvatarNFT.deployed();
+        const nft = await TemplateNFT.deployed();
         // mint
         try {
-            await nft.mint(1, { from: accounts[1], value: 0.03 * ether });
+            await nft.mint(1, { from: accounts[1], value: 1 * ether });
         } catch (error) {
             // check that error message has expected substring 'Sale not started'
             assert.include(error.message, "Sale not started");
@@ -30,32 +30,32 @@ contract("AvatarNFT", accounts => {
 
     // it should mint successfully
     it("should mint successfully when sale is started", async () => {
-        const nft = await AvatarNFT.deployed();
+        const nft = await TemplateNFT.deployed();
         // flipSaleStarted
         await nft.flipSaleStarted();
         // mint
-        const tx = await nft.mint(1, { from: accounts[0], value: 0.03 * ether });
+        const tx = await nft.mint(1, { from: accounts[0], value: 1 * ether });
         assert.ok(tx);
     });
 
     // it should be able to mint 10 tokens in one transaction
     it("should be able to mint 10 tokens in one transaction", async () => {
-        const nft = await AvatarNFT.deployed();
+        const nft = await TemplateNFT.deployed();
         // flipSaleStarted
         // await nft.flipSaleStarted();
         // mint
         const nTokens = 10;
-        const tx = await nft.mint(nTokens, { from: accounts[0], value: 0.03 * nTokens * ether });
+        const tx = await nft.mint(nTokens, { from: accounts[0], value: 1 * nTokens * ether });
         assert.ok(tx);
     });
 
     // it should fail trying to mint more than 20 tokens
     it("should fail trying to mint more than 20 tokens", async () => {
-        const nft = await AvatarNFT.deployed();
+        const nft = await TemplateNFT.deployed();
 
         // mint
         try {
-            await nft.mint(21, { from: accounts[0], value: 0.03 * 21 * ether });
+            await nft.mint(21, { from: accounts[0], value: 1 * 21 * ether });
         } catch (error) {
             // check that error message has expected substring 'You cannot mint more than'
             assert.include(error.message, "You cannot mint more than");
@@ -64,16 +64,16 @@ contract("AvatarNFT", accounts => {
 
     // it should be able to mint when you send more ether than needed
     it("should be able to mint when you send more ether than needed", async () => {
-        const nft = await AvatarNFT.deployed();
+        const nft = await TemplateNFT.deployed();
 
         // mint
-        const tx = await nft.mint(1, { from: accounts[0], value: 0.5 * ether });
+        const tx = await nft.mint(1, { from: accounts[0], value: 1.5 * ether });
         assert.ok(tx);
     });
 
     // it should be able to change baseURI from owner account, and _baseURI() value would change
     it("should be able to change baseURI from owner account, and _baseURI() value would change", async () => {
-        const nft = await AvatarNFT.deployed();
+        const nft = await TemplateNFT.deployed();
 
         const baseURI = "https://avatar.com/";
         await nft.setBaseURI(baseURI, { from: accounts[0] });
@@ -84,7 +84,7 @@ contract("AvatarNFT", accounts => {
 
     // it should be able to mint reserved from owner account
     it("should be able to mint reserved from owner account", async () => {
-        const nft = await AvatarNFT.deployed();
+        const nft = await TemplateNFT.deployed();
 
         // mint
         const tx = await nft.claimReserved(3, accounts[1], { from: accounts[0] });
@@ -93,7 +93,7 @@ contract("AvatarNFT", accounts => {
 
     // it should not be able to mint reserved from accounts other that owner
     it("should not be able to mint reserved from accounts other that owner", async () => {
-        const nft = await AvatarNFT.deployed();
+        const nft = await TemplateNFT.deployed();
 
         // mint
         try {
@@ -104,20 +104,20 @@ contract("AvatarNFT", accounts => {
         }
     });
 
-    it("should not be able to mint more than 500 tokens, when 500 tokens are minted, it should fail", async () => {
-        const nft = await AvatarNFT.deployed();
+    // // when 10000 tokens are minted, it should fail
+    // it("when 10000 tokens are minted, it should fail", async () => {
+    //     const nft = await TemplateNFT.deployed();
 
-        // set price to 0.0001 ether
-        await nft.setPrice(0.0001 * ether);
+    //     // mint
+    //     try {
+    //         // try minting 20000 tokens, which is more than the max allowed
+    //         for (let i = 0; i < 1000; i++) {
+    //             await nft.mint(20, { from: accounts[0], value: 1 * 20 * ether });
+    //         }
+    //     } catch (error) {
+    //         // check that error message has expected substring 'You cannot mint more than'
+    //         assert.include(error.message, "You cannot mint more than");
+    //     }
+    // })
 
-        // try minting 20 * 101 tokens, which is more than the max allowed
-        try {
-            await Promise.all(Array(50).fill().map(() =>
-                nft.mint(20, { from: accounts[0], value: 0.0001 * 20 * ether })
-            ));
-        } catch (error) {
-            // check that error message has expected substring 'You cannot mint more than'
-            assert.include(error.message, "Not enough Tokens left");
-        }
-    })
 })
