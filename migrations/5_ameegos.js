c9f843d059fdfce96b5934815ad912f7c086f3ee
Aleksey Bykhun <theisease@gmail.com>
2021-10-22 00:45:25 +0800

    ameegos: fix onchain metadata for opensea

--- migrations/5_ameegos.js
+++ migrations/5_ameegos.js
@@ -1,6 +1,7 @@
 const AmeegosExtras = artifacts.require("AmeegosExtras");
 const Market = artifacts.require("Market");
-const AvatarNFT = artifacts.require("AvatarNFT");
+
+const owner = "0x2195601e1EA42363C85AC7868143b80d20Db978f";
 
 module.exports = async function(deployer) {
   // deployer.deploy(AvatarNFT, "30000000000000000", 500, 200, 20, "https://metadata.buildship.dev/", "Avatar Collection NFT", "NFT");
@@ -12,4 +13,25 @@ module.exports = async function(deployer) {
   const extras = await AmeegosExtras.deployed();
 
   await deployer.deploy(Market, extras.address);
+
+  // price   uint256 :  1000000000000000
+  // maxSupply   uint256 :  1000
+  // mintedSupply   uint256 :  3
+  // name   string :  Dirty Stone Skin
+  // imageUrl   string :  https://www.vizpark.com/wp-content/uploads/2018/06/VP-Stone-floor-1-cam-2-870x489.jpg
+
+  await extras.addItem("Dirty Stone Skin", "https://www.vizpark.com/wp-content/uploads/2018/06/VP-Stone-floor-1-cam-2-870x489.jpg", "10000000000000000", 1000, true);
+
+  // price   uint256 :  3000000000000000
+  // maxSupply   uint256 :  2000
+  // mintedSupply   uint256 :  5
+  // name   string :  Pretty Carpet Skin
+  // imageUrl   string :  https://i.pinimg.com/originals/23/bc/15/23bc157ee8f708b216a6d386de51460c.jpg
+
+  await extras.addItem("Pretty Carpet Skin", "https://i.pinimg.com/originals/23/bc/15/23bc157ee8f708b216a6d386de51460c.jpg", "3000000000000000", 2000, true);
+
+  await extras.buyItem(0, 1, {value: "10000000000000000"});
+
+  await extras.transferOwnership(owner);
+
 };
