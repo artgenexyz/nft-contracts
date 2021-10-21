ab44bb68cf1eb3f832cf504309081339dd14c98e
Aleksey Bykhun <theisease@gmail.com>
2021-10-21 21:53:43 +0800

    mintpass: add into view

--- contracts/WithMintPass.sol
+++ contracts/WithMintPass.sol
@@ -47,4 +47,44 @@ contract WithMintPass {
 
         _;
     }
+
+    function hasAvailable(address buyer) public view returns (uint256) {
+
+        if (mintPass.balanceOf(buyer) == 0) {
+            return 0;
+        }
+
+        if (logic == MintPassLogic.LimitAddress) {
+            return MAX_PER_MINT_PASS - mintPassUsedBy[buyer];
+        }
+
+        if (logic == MintPassLogic.LimitTokenId) {
+            if(!mintPass.supportsInterface(type(IERC721Enumerable).interfaceId)) {
+                require(false, "Unsupported token type");
+            }
+
+            uint256 balance = IERC721Enumerable(address(mintPass)).balanceOf(buyer);
+
+            uint256 available = balance * MAX_PER_MINT_PASS;
+
+            for (uint256 index = 0; index < balance; index++) {
+                // uint256 tokenId = IERC721Enumerable(address(mintPass)).tokenOfOwnerByIndex(msg.sender, index);
+                uint256 tokenId = IERC721Enumerable(address(mintPass)).tokenOfOwnerByIndex(buyer, index);
+
+                available -= mintPassUsedByTokenId[tokenId];
+            }
+
+            return available;
+
+        }
+
+        if (logic == MintPassLogic.LimitBoth) {
+            // TODO
+
+
+            return 0;
+        }
+
+        return 0;
+    }
 }
