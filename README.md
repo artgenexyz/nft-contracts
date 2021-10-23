4452d56399d8001082d45c933e67b79eeaeda508
Aleksey Bykhun <theisease@gmail.com>
2021-10-23 16:02:20 +0800

    ameegos: add mintpass, add mainnet hack for minting

--- README.md
+++ README.md
@@ -49,3 +49,25 @@ How to interact with contracts from deployed truffle?
 truffle console --network rinkeby
 ```
 
+### Mainnet fork
+
+We have a special test for `AmeegosMintPass`. It runs on testnet, but it's best to run on mainnet fork too.
+
+Run ganache to fork from mainnet:
+
+```bash
+source .env
+npx ganache-cli --fork https://mainnet.infura.io/v3/$INFURA_KEY\
+                --unlock 0x44244acacd0b008004f308216f791f2ebe4c4c50
+```
+
+0x4424 is an address of original AmeegosNFT owner.
+
+Then, run test suite for mainnet fork. It will automatically detect we're on forked version, and will not deploy new contract, instead attach to the old one.
+
+```bash
+truffle test test/mintpass_mainnet_fork.js
+```
+
+
+
--- migrations/5_ameegos.js
+++ migrations/5_ameegos.js
@@ -1,37 +1,40 @@
 const AmeegosExtras = artifacts.require("AmeegosExtras");
 const Market = artifacts.require("Market");
 
+const AmeegosNFT = artifacts.require("AmeegosNFT");
+const AmeegosMintPass = artifacts.require("AmeegosMintPass");
+
+const AMEEGOS_ADMIN = "0x44244acaCD0B008004F308216f791F2EBE4C4C50";
 const owner = "0x2195601e1EA42363C85AC7868143b80d20Db978f";
 
 module.exports = async function(deployer) {
-  // deployer.deploy(AvatarNFT, "30000000000000000", 500, 200, 20, "https://metadata.buildship.dev/", "Avatar Collection NFT", "NFT");
-
-  // const mintPass = await AvatarNFT.new("3000000000000000", 500, 200, 20, "https://mintpass.io", "Test Mint Pass", "MINTPASS");
 
   await deployer.deploy(AmeegosExtras);
-
   const extras = await AmeegosExtras.deployed();
-
   await deployer.deploy(Market, extras.address);
 
-  // price   uint256 :  1000000000000000
-  // maxSupply   uint256 :  1000
-  // mintedSupply   uint256 :  3
-  // name   string :  Dirty Stone Skin
-  // imageUrl   string :  https://www.vizpark.com/wp-content/uploads/2018/06/VP-Stone-floor-1-cam-2-870x489.jpg
-
   await extras.addItem("Dirty Stone Skin", "https://www.vizpark.com/wp-content/uploads/2018/06/VP-Stone-floor-1-cam-2-870x489.jpg", "10000000000000000", 1000, true);
-
-  // price   uint256 :  3000000000000000
-  // maxSupply   uint256 :  2000
-  // mintedSupply   uint256 :  5
-  // name   string :  Pretty Carpet Skin
-  // imageUrl   string :  https://i.pinimg.com/originals/23/bc/15/23bc157ee8f708b216a6d386de51460c.jpg
-
   await extras.addItem("Pretty Carpet Skin", "https://i.pinimg.com/originals/23/bc/15/23bc157ee8f708b216a6d386de51460c.jpg", "3000000000000000", 2000, true);
-
   await extras.buyItem(0, 1, {value: "10000000000000000"});
 
   await extras.transferOwnership(owner);
+
+  await deployer.deploy(AmeegosNFT, "Ameegos Metaverse", "AMEEGOS", "uri://test", "uri://test");
+
+  const nft = await AmeegosNFT.deployed();
+
+  const mintPass = await AmeegosMintPass.new(nft.address);
+
+  console.log("mint pass deployed to", mintPass.address);
+
+  await mintPass.flipSaleStarted();
+  await mintPass.transferOwnership(AMEEGOS_ADMIN);
+
+  await nft.whitelistUsers([mintPass.address]);
+  await nft.setOnlyWhitelisted(true);
+  await nft.setNftPerAddressLimit(6000); // because MintPass needs to be able to mint from their address
+  await nft.pause(false);
+
+  await nft.transferOwnership(AMEEGOS_ADMIN);
 
 };
