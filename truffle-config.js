3cb795ec996df6111b2a4491e45c57ec7a9e6ca8
Aleksey Bykhun <theisease@gmail.com>
2021-10-26 19:55:53 +0800

    truffle: add mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -32,15 +32,6 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
@@ -52,6 +43,16 @@ module.exports = {
       skipDryRun: true,
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       networkCheckTimeout: 30000, // If you have a slow internet connection, try configuring a longer timeout in your Truffle config.
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${INFURA_KEY}`),
+      network_id: 1,
+      gas: 8500000,
+      gasPrice: 50e9, // 50 gwei
+      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+      skipDryRun: false,
+      timeoutBlocks: 200,
+      networkCheckTimeout: 60000, // If you have a slow internet connection, try configuring a longer timeout in your Truffle config.
     },
     polygon: {
       provider: () => new HDWalletProvider({
