c8c40a0b0c939a7fffa935b0585426fb815cf773
Aleksey Bykhun <theisease@gmail.com>
2021-12-09 18:30:55 +0800

    factory: fix tests, add payment

--- contracts/factory/NFTFactory.sol
+++ contracts/factory/NFTFactory.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// import Clones
 import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 import "./SharedImplementationNFT.sol";
 
-contract NFTFactory {
+contract NFTFactory is Ownable {
 
     address public immutable proxyImplementation;
 
@@ -22,7 +23,9 @@ contract NFTFactory {
         uint256 _maxTokensPerMint,
         string memory _projectName,
         string memory _name, string memory _symbol
-    ) external {
+    ) external payable {
+        require(msg.value >= cost(), "Not enough payment");
+
         address clone = Clones.clone(proxyImplementation);
 
         SharedImplementationNFT(clone).initialize(
@@ -36,6 +39,28 @@ contract NFTFactory {
         SharedImplementationNFT(clone).transferOwnership(msg.sender);
 
         emit NFTCreated(clone);
+
+        Address.sendValue(payable(msg.sender), msg.value - cost());
+    }
+
+    function cost() public view returns (uint256) {
+        // we aim to be 10 times cheaper than deploying your own smart-contract
+        uint approxDeployCost = 5_000_000 * block.basefee;
+
+        // we ignore priority_fee
+
+        // uint gasCost = 334_044; // TODO: record estimation
+        // uint gasCost = 314_132;
+
+        // so we charge 500_000 gas on top of 330_000 gas you pay for the deploy
+
+        return approxDeployCost / 10;
+    }
+
+    function withdraw() public onlyOwner {
+        uint balance = address(this).balance;
+
+        Address.sendValue(payable(msg.sender), balance);
     }
 
 }
--- test/utils/index.js
+++ test/utils/index.js
@@ -1,9 +1,7 @@
-const { web3 } = require("@openzeppelin/test-helpers/src/setup");
 const BigNumber = require("bignumber.js");
 
 const getGasCost = async tx => {
-    // TODO: is there way to take real gas price?
-    return new BigNumber(tx.receipt.gasUsed).times(await web3.eth.getGasPrice());
+    return new BigNumber(tx.receipt.gasUsed).times(tx.receipt.effectiveGasPrice);
 }
 
 module.exports = { getGasCost }
