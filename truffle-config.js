174e271e1f05bfb50455199c344f530a1aef4473
Aleksey Bykhun <theisease@gmail.com>
2021-12-12 19:51:37 +0800

    live: The Cult DAO

--- truffle-config.js
+++ truffle-config.js
@@ -54,7 +54,7 @@ module.exports = {
       provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${INFURA_KEY}`),
       network_id: 1,
       gas: 8500000,
-      gasPrice: 100e9,
+      gasPrice: 36e9,
       confirmations: 1,    // # of confs to wait between deployments. (default: 0)
       skipDryRun: false,
       timeoutBlocks: 200,
