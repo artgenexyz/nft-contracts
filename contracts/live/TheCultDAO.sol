5383dc7590669297a715254dc2737b8d218afe34
Aleksey Bykhun <theisease@gmail.com>
2021-12-14 01:17:46 +0800

    thecultdao: polygon version

--- contracts/live/TheCultDAO.sol
+++ contracts/live/TheCultDAO.sol
@@ -6,11 +6,11 @@ import "../AvatarNFT.sol";
 contract TheCultDAO is AvatarNFT {
 
     constructor() AvatarNFT(
-        0.05 ether,
-        20200, // total supply
+        100 ether, // MATIC
+        10100, // total supply
         700, // reserved supply
         50, // max mint per transaction
-        "https://metadata.buildship.dev/api/token/thecultdao/",
+        "https://metadata.buildship.dev/api/token/thecultdao-rock/",
         "The Cult DAO", "CULD"
     ) {}
 
