3dad189fc102de39a396aff6872d37abae5384d1
Aleksey Bykhun <theisease@gmail.com>
2021-12-17 19:57:08 +0800

    factory: add erc165 for imetaversenft, remove claim(), add opensea support library

--- contracts/factory/OpenseaProxy.sol
+++ contracts/factory/OpenseaProxy.sol
@@ -10,3 +10,20 @@ interface OwnableDelegateProxy {
 interface ProxyRegistry {
     function proxies(address) external view returns (OwnableDelegateProxy);
 }
+
+library SupportsOpensea {
+
+    // Use like this:
+
+    // if (isOpenSeaProxyActive && SupportsOpensea.isApprovedForAll(owner, operator)) {
+    //     return true;
+    // }
+
+    function isApprovedForAll(address owner, address operator) public view returns (bool) {
+        // Get a reference to OpenSea's proxy registry contract by instantiating
+        // the contract using the already existing address.
+        ProxyRegistry proxyRegistry = ProxyRegistry(0xa5409ec958C83C3f309868babACA7c86DCB077c1);
+
+        return address(proxyRegistry.proxies(owner)) == operator;
+    }
+}
