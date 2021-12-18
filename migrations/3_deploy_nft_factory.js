390706d0b7da7026d8db19a166fef2cd844e50ef
Aleksey Bykhun <theisease@gmail.com>
2021-12-19 03:00:18 +0800

    migrations: verify network

--- migrations/3_deploy_nft_factory.js
+++ migrations/3_deploy_nft_factory.js
@@ -1,6 +1,6 @@
 const NFTFactory = artifacts.require("MetaverseNFTFactory");
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network, accounts) {
 
   if (!process.env.FACTORY_NOT_SKIP) return;
 
@@ -13,7 +13,7 @@ module.exports = async function (deployer) {
   console.log('   > implementation created at address:', implementation);
 
   console.log('   -------------------------------------')
-  console.log('   Verify:\n\t', `truffle run verify MetaverseNFTFactory MetaverseNFT@${implementation} --network rinkeby`, '\n')
+  console.log('   Verify:\n\t', `truffle run verify MetaverseNFTFactory MetaverseNFT@${implementation} --network ${network}`, '\n')
 
   console.log('   -------------------------------------')
 
