f3808137491e6cf79952ec4451d9a457d9c38c3a
Aleksey Bykhun <theisease@gmail.com>
2021-12-19 02:04:10 +0800

    factory: fix ArtFactory

--- contracts/factory/ArtNFTFactory.sol
+++ contracts/factory/ArtNFTFactory.sol
@@ -5,7 +5,45 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
-import "./MetaverseNFT.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+// TODO: support Foundation format
+contract ArtNFT is ERC721Upgradeable, OwnableUpgradeable {
+
+    string private baseURI;
+
+    function initialize(
+        string memory _uri,
+        string memory _name, string memory _symbol
+    ) public initializer {
+        __ERC721_init(_name, _symbol);
+        // __ERC721Burnable_init();
+        // __ReentrancyGuard_init();
+        __Ownable_init();
+
+        baseURI = _uri;
+    }
+
+    // This constructor ensures that this contract can only be used as a master copy
+    // Marking constructor as initializer makes sure that real initializer cannot be called
+    // Thus, as the owner of the contract is 0x0, no one can do anything with the contract
+    // on the other hand, it's impossible to call this function in proxy,
+    // so the real initializer is the only initializer
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function mint(address _to, uint256 _tokenId) public onlyOwner {
+        _mint(_to, _tokenId);
+    }
+
+    // TODO: mint via extension, add extension etc
+}
 
 contract ArtNFTFactory is Ownable {
 
@@ -14,13 +52,13 @@ contract ArtNFTFactory is Ownable {
     event NFTCreated(address deployedAddress);
 
     constructor() {
-        proxyImplementation = address(new MetaverseNFT());
+        proxyImplementation = address(new ERC721Upgradeable());
     }
 
     function createNFT(
         // uint256 _startPrice,
-        uint256 _maxSupply,
-        uint256 _nReserved,
+        // uint256 _maxSupply,
+        // uint256 _nReserved,
         // uint256 _maxTokensPerMint,
         string memory _uri,
         string memory _name, string memory _symbol
@@ -29,16 +67,16 @@ contract ArtNFTFactory is Ownable {
 
         address clone = Clones.clone(proxyImplementation);
 
-        MetaverseNFT(clone).initialize(
+        ArtNFT(clone).initialize(
             // _startPrice,
-            _maxSupply,
-            _nReserved,
+            // _maxSupply,
+            // _nReserved,
             // _maxTokensPerMint,
             _uri,
             _name, _symbol
         );
 
-        MetaverseNFT(clone).transferOwnership(msg.sender);
+        ArtNFT(clone).transferOwnership(msg.sender);
 
         emit NFTCreated(clone);
 
