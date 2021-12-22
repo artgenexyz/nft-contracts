bc7e01f8232cab32765e617ea543310abcb28037
tylig <erin788@mail.ru>
2021-12-22 17:26:49 +0700

    - rename amountOfAllTokens => supply

--- migrations/3_deploy_nft_factory.js
+++ migrations/3_deploy_nft_factory.js
@@ -1,24 +1,26 @@
 const NFTFactory = artifacts.require("MetaverseNFTFactory");
+const ArtNFTFactory = artifacts.require("ArtNFTFactory");
 
 module.exports = async function (deployer, network, accounts) {
 
   if (!process.env.FACTORY_NOT_SKIP) return;
 
   await deployer.deploy(NFTFactory);
+  await deployer.deploy(ArtNFTFactory);
+    
+    const factory = await NFTFactory.deployed();
+    const artFactory = await ArtNFTFactory.deployed(); 
+    const implementation = await factory.proxyImplementation();
 
-  const factory = await NFTFactory.deployed();
+    console.log('   > implementation created at address:', implementation);
 
-  const implementation = await factory.proxyImplementation();
+    console.log('   -------------------------------------')
+    console.log('   Verify:\n\t', `truffle run verify MetaverseNFTFactory MetaverseNFT@${implementation} --network ${network}`, '\n')
 
-  console.log('   > implementation created at address:', implementation);
+    console.log('   -------------------------------------')
 
-  console.log('   -------------------------------------')
-  console.log('   Verify:\n\t', `truffle run verify MetaverseNFTFactory MetaverseNFT@${implementation} --network ${network}`, '\n')
+    // const [ nftCreated ] = factory.contract.getPastEvents('NFTCreated', { fromBlock: 0, toBlock: 'latest' });
 
-  console.log('   -------------------------------------')
+    // console.log('nft created:', nftCreated?.returnValues?.deployedAddress);
 
-  // const [ nftCreated ] = factory.contract.getPastEvents('NFTCreated', { fromBlock: 0, toBlock: 'latest' });
-
-  // console.log('nft created:', nftCreated?.returnValues?.deployedAddress);
-
-};
+  };
