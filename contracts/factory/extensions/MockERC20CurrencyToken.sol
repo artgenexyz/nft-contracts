8d5b5ba069a36de55ba5dcbe7208320995f1734f
Grigory Ivanov <crewsquirrel@gmail.com>
2022-02-02 19:23:17 +0200

    Extension to buy NFT's by ERC20 tokens

---
+++ contracts/factory/extensions/MockERC20CurrencyToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+
+contract MockERC20CurrencyToken is ERC20PresetFixedSupply {
+    constructor () ERC20PresetFixedSupply("testCurrency", "TC", 10000, msg.sender) {}
+}
