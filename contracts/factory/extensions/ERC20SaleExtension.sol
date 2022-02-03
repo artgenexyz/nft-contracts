d695a26ad3f10dbb6fd19d0be4cebed03154327d
Grigory Ivanov <crewsquirrel@gmail.com>
2022-02-03 11:50:14 +0200

    Added currency and price change with event + tests

--- contracts/factory/extensions/ERC20SaleExtension.sol
+++ contracts/factory/extensions/ERC20SaleExtension.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./NFTExtension.sol";
 
-contract ERC20SaleExtension is NFTExtension {
+contract ERC20SaleExtension is NFTExtension, Ownable {
 
     using SafeERC20 for IERC20;
 
@@ -15,11 +16,15 @@ contract ERC20SaleExtension is NFTExtension {
     uint256 public price;
     uint256 public maxPerMint;
 
+    event currencyChanged(address indexed newCurrency, uint256 newPrice);
+
     // Currently looking for solution how to check if _currencyTokenAddress is address of valid ERC20 contract
     constructor(address _nft, address _currencyAddress, uint256 _price, uint256 _maxPerMint) NFTExtension(_nft) {
         currency = IERC20(_currencyAddress);
         price = _price;
         maxPerMint = _maxPerMint;
+
+        emit currencyChanged(_currencyAddress, _price);
     }
 
     // In order to mint user have to approve ERC20 tokens to extension address
@@ -32,5 +37,13 @@ contract ERC20SaleExtension is NFTExtension {
 
         currency.safeTransferFrom(msg.sender, address(nft), currencyPrice);
         nft.mintExternal(nTokens, msg.sender, bytes32(0x0));
+    }
+
+    function changeCurrency(address _newCurrency, uint256 _newPrice) external onlyOwner {
+        require(_newPrice > 0, "New price must be bigger then zero");
+        currency = IERC20(_newCurrency);
+        price = _newPrice;
+
+        emit currencyChanged(_newCurrency, _newPrice);
     }
 }
