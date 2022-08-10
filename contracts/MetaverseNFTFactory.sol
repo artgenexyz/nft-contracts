28c859015badee5d5b1d1c7c1f43c1bbcf34c59f
Aleksey Bykhun <theisease@gmail.com>
2022-08-10 20:17:39 +0200

    remove extra import

--- contracts/MetaverseNFTFactory.sol
+++ contracts/MetaverseNFTFactory.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./MetaverseNFT.sol";
 
-import "./MetaverseNFTProxy.sol";
-
 /**
  * MetaverseNFT is a cloneable contract for your NFT collection.
  * It's adapted from OpenZeppeling ERC721 implementation upgradeable versions.
