64d6d83af21bd6ee42813783fe20b848392b30d1
Aleksey Bykhun <theisease@gmail.com>
2022-08-25 21:19:22 +0200

    fix comments

--- contracts/interfaces/IMetaverseNFT.sol
+++ contracts/interfaces/IMetaverseNFT.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-/** @dev these values can be edited later */
+/** @dev config includes values have setters and can be changed later */
 struct MintConfig {
     uint256 publicPrice;
     uint256 maxTokensPerMint;
