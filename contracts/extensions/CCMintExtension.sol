0e35b69f07cc98893a92ac2222b99927c4abb5d3
Aleksey Bykhun <theisease@gmail.com>
2022-10-13 01:16:35 +0200

    add support for paper.xyz

--- contracts/extensions/CCMintExtension.sol
+++ contracts/extensions/CCMintExtension.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@paperxyz/contracts/keyManager/IPaperKeyManager.sol";
 
 import "./base/NFTExtension.sol";
 
@@ -9,6 +10,8 @@ contract CCMintExtension is NFTExtension, Ownable {
     uint256 public price;
     uint256 public maxPerMint;
     mapping(address => bool) public minters;
+
+    IPaperKeyManager public paperKM;
 
     constructor(
         address _nft,
@@ -34,11 +37,61 @@ contract CCMintExtension is NFTExtension, Ownable {
         }
     }
 
-    function mint(uint256 amount, address recipient) external payable {
+    modifier onlyMinters() {
+        // check that msg.sender is one of minters
+        require(minters[msg.sender], "CCMintExtension: not a minter");
+        _;
+    }
+
+    function setPaperKeyManager(address _paperKeyManagerAddress)
+        external
+        onlyOwner
+    {
+        paperKM = IPaperKeyManager(_paperKeyManagerAddress);
+    }
+
+    function registerPaperKey(address _paperKey) external onlyOwner {
+        require(paperKM.register(_paperKey), "Error registering key");
+    }
+
+    // onlyPaper modifier to easily restrict multiple different function
+    modifier onlyPaper(
+        bytes32 _hash,
+        bytes32 _nonce,
+        bytes calldata _signature
+    ) {
+        bool success = paperKM.verify(_hash, _nonce, _signature);
+        require(success, "CCMintExtension: Failed to verify paper.xyz signature");
+        _;
+    }
+
+    // --- Minting ---
+
+    // winter
+    function mint(uint256 amount, address recipient)
+        external
+        payable
+        onlyMinters
+    {
         super.beforeMint();
 
-        // check that msg.sender is one of minters
-        require(minters[msg.sender] == true, "Not an allowed minter");
+        require(msg.value >= price * amount, "Not enough ETH to mint");
+
+        nft.mintExternal(amount, recipient, bytes32(0x0));
+    }
+
+    // paper.xyz
+    function mint(
+        uint256 amount,
+        address recipient,
+        bytes32 _nonce,
+        bytes calldata _signature
+    )
+        external
+        payable
+        onlyPaper(keccak256(abi.encode(amount, recipient)), _nonce, _signature)
+    {
+        super.beforeMint();
 
         require(msg.value >= price * amount, "Not enough ETH to mint");
 
