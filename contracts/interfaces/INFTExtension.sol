862ac37672990e1f70223523a5e7661cca60abb0
Aleksey Bykhun <theisease@gmail.com>
2022-10-25 19:06:26 +0300

    draft transfer extension

--- contracts/interfaces/INFTExtension.sol
+++ contracts/interfaces/INFTExtension.sol
@@ -5,6 +5,22 @@ import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 interface INFTExtension is IERC165 {}
 
+interface IERC721CommunityExtension {
+    function community() external view returns (address);
+}
+
 interface INFTURIExtension is INFTExtension {
     function tokenURI(uint256 tokenId) external view returns (string memory);
+}
+
+interface IERC721CommunityTokenURIExtension is INFTURIExtension {
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+}
+
+interface IERC721CommunityBeforeTransferExtension is INFTExtension {
+    function beforeTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
 }
