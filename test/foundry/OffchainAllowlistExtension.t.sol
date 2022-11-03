42efba92b04f5f1f1fc7aebf8052477eb5e3c38c
Aleksey Bykhun <theisease@gmail.com>
2022-11-03 22:46:58 +0300

    fix tests

--- test/foundry/OffchainAllowlistExtension.t.sol
+++ test/foundry/OffchainAllowlistExtension.t.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.9;
 
 import "forge-std/Test.sol";
 
-import "../../contracts/ERC721CommunityBase.sol";
-import "../../contracts/extensions/OffchainAllowlistExtension.sol";
+import "contracts/ERC721CommunityBase.sol";
+import "contracts/extensions/OffchainAllowlistExtension.sol";
 
 contract OffchainAllowlistExtensionTest is Test {
     OffchainAllowlistExtension public extension;
