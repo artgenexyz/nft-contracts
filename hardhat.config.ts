a7ce7de474f993c7d017a59c9dd1ed5ede2c7b6e
Aleksey Bykhun <theisease@gmail.com>
2023-04-24 14:46:43 -0400

    update solidity, openzeppelin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -115,7 +115,7 @@ const config: HardhatUserConfig = {
         },
     },
     solidity: {
-        version: "0.8.9",
+        version: "0.8.18",
         settings: {
             optimizer: {
                 enabled: true,
