eb0868ba4389db11cd8e05c46b26b35ffe1d0bcb
Aleksey Bykhun <theisease@gmail.com>
2023-06-12 21:04:50 -0400

    deploy InformationAge

--- scripts/deploy-onchain-ext.ts
+++ scripts/deploy-onchain-ext.ts
@@ -1,3 +1,4 @@
+import { ArtgeneScript } from "./../typechain-types/contracts/extensions/OnchainArtStorageExtensionScripty.sol/ArtgeneScript";
 // import { deployedContracts } from 'scripty.sol';
 // import { Base64Converter } from "../typechain-types/contracts/extensions/OnchainArtStorageExtension.sol/Base64Converter";
 import fs from "fs";
@@ -31,11 +32,13 @@ const deployedContracts = {
     ScriptyStorage: "0x096451F43800f207FC32B4FF86F286EdaF736eE3",
     ScriptyBuilder: "0x16b727a2Fc9322C724F4Bc562910c99a5edA5084",
     ETHFSFileStorage: "0xFc7453dA7bF4d0c739C1c53da57b3636dAb0e11e",
+    ArtgeneScript: "0xA9130dd87b0DAf3c18A11397aAF79f72a36676fc",
   },
   goerli: {
     ScriptyStorage: "0x730B0ADaaD15B0551928bAE7011F2C1F2A9CA20C",
     ScriptyBuilder: "0xc9AB9815d4D5461F3b53Ebd857b6582E82A45C49",
     ETHFSFileStorage: "0x70a78d91A434C1073D47b2deBe31C184aA8CA9Fa",
+    ArtgeneScript: "",
   },
 };
 
@@ -144,25 +147,28 @@ const main = async () => {
   }
 
   console.log("NFT contract address is", nft);
-  //   const lib = await hre.ethers.getContractFactory("Base64Converter");
-  //   const libDeployed = await lib.deploy();
-  //   await libDeployed.deployed();
-  //   console.log("Base64Converter deployed to:", libDeployed.address);
+  const network = hre.network.name as keyof typeof deployedContracts;
+  if (!deployedContracts[network]?.ArtgeneScript) {
+    //   const lib = await hre.ethers.getContractFactory("Base64Converter");
+    //   const libDeployed = await lib.deploy();
+    //   await libDeployed.deployed();
+    //   console.log("Base64Converter deployed to:", libDeployed.address);
 
-  // deploy ArtgeneScript
-  const ArtgeneScript = await hre.ethers.getContractFactory("Artgene_js");
-  const artgeneScript = await ArtgeneScript.deploy();
-  await artgeneScript.deployed();
-  console.log("ArtgeneScript deployed to:", artgeneScript.address);
+    // deploy ArtgeneScript
+    const ArtgeneScript = await hre.ethers.getContractFactory("Artgene_js");
+    const artgeneScript = await ArtgeneScript.deploy();
+    await artgeneScript.deployed();
+    console.log("ArtgeneScript deployed to:", artgeneScript.address);
 
-  await calculateCosts(artgeneScript.deployTransaction);
+    await calculateCosts(artgeneScript.deployTransaction);
 
-  if (hre.network.name === "goerli" || hre.network.name === "mainnet") {
-    // dont wait for verification
-    hre.run("verify:verify", {
-      address: artgeneScript.address,
-      constructorArguments: [],
-    });
+    if (hre.network.name === "goerli" || hre.network.name === "mainnet") {
+      // dont wait for verification
+      await hre.run("verify:verify", {
+        address: artgeneScript.address,
+        constructorArguments: [],
+      });
+    }
   }
 
   // deploy OnchainArtStorageExtension.sol
---
+++ scripts/onchain/information-age-params.js
@@ -0,0 +1,25 @@
+const deployedContracts = {
+  mainnet: {
+    ScriptyStorage: "0x096451F43800f207FC32B4FF86F286EdaF736eE3",
+    ScriptyBuilder: "0x16b727a2Fc9322C724F4Bc562910c99a5edA5084",
+    ETHFSFileStorage: "0xFc7453dA7bF4d0c739C1c53da57b3636dAb0e11e",
+  },
+  goerli: {
+    ScriptyStorage: "0x730B0ADaaD15B0551928bAE7011F2C1F2A9CA20C",
+    ScriptyBuilder: "0xc9AB9815d4D5461F3b53Ebd857b6582E82A45C49",
+    ETHFSFileStorage: "0x70a78d91A434C1073D47b2deBe31C184aA8CA9Fa",
+  },
+};
+
+const artgeneScript = {
+  address: "0xA9130dd87b0DAf3c18A11397aAF79f72a36676fc",
+};
+
+module.exports = [
+  "0xb276D2184F6AaDA58BDb6526d86f00Fc8543c26C",
+  artgeneScript.address,
+
+  deployedContracts.mainnet.ETHFSFileStorage,
+  deployedContracts.mainnet.ScriptyStorage,
+  deployedContracts.mainnet.ScriptyBuilder,
+];
