5065707f36288f85d5aab6f654ede57d62e25811
Aleksey Bykhun <theisease@gmail.com>
2023-07-06 11:15:48 -0400

    tasks: adapt for mainnet deploy

--- tasks/deploy-contract.ts
+++ tasks/deploy-contract.ts
@@ -8,80 +8,93 @@ import { formatEther } from "ethers/lib/utils";
 task("deploy-contract", "Deploys a contract")
   .addPositionalParam("contractName", "The contract name")
   .addOptionalParam("args", "The contract arguments", "[]", types.json)
-  .setAction(async ({ contractName, args }, hre) => {
-    console.log(
-      `Running deploy script for the network ${hre.network.name}`,
-      "with args:",
-      args
-    );
-
-    try {
-      const factory = await hre.ethers.getContractFactory(contractName);
-      const artifact = await hre.artifacts.readArtifact(contractName);
-      const contract = await(factory as any).deploy(...args);
-
-      console.log(`Deployed ${contractName} to: ${contract.address}`);
-
-      // save args to a file
-      const argsFile = join(
-        hre.config.paths.artifacts,
-        "..",
-        "args-" + contractName + "@" + contract.address + ".json"
+  .addOptionalParam("gasPrice", "The gas price in gwei", "", types.string)
+  .addOptionalParam("gasLimit", "The gas limit", "", types.string)
+  .addOptionalParam("overrideNonce", "Override the nonce", "", types.string)
+  .setAction(
+    async ({ contractName, args, gasPrice, gasLimit, overrideNonce }, hre) => {
+      console.log(
+        `Running deploy script for the network ${hre.network.name}`,
+        "with args:",
+        args
       );
 
-      await writeFile(argsFile, JSON.stringify(args, null, 2));
+      try {
+        const factory = await hre.ethers.getContractFactory(contractName);
+        const artifact = await hre.artifacts.readArtifact(contractName);
 
-      console.log("Args saved to:", argsFile);
+        const contract = await (factory as any).deploy(...args, {
+          maxFeePerGas: gasPrice
+            ? hre.ethers.utils.parseUnits(gasPrice, "gwei")
+            : undefined,
+          maxPriorityFeePerGas: hre.ethers.utils.parseUnits("2", "gwei"),
+          gasLimit: gasLimit ? parseInt(gasLimit) : undefined,
+          nonce: overrideNonce ? parseInt(overrideNonce) : undefined,
+        });
 
-      await contract.deployed();
+        console.log(`Deployed ${contractName} to: ${contract.address}`);
 
-      const tx = contract.deployTransaction;
+        // save args to a file
+        const argsFile = join(
+          hre.config.paths.artifacts,
+          "..",
+          "args-" + contractName + "@" + contract.address + ".json"
+        );
 
-      const receipt = await tx.wait();
+        await writeFile(argsFile, JSON.stringify(args, null, 2));
 
-      const contractFullName = `${artifact.sourceName}:${artifact.contractName}`;
+        console.log("Args saved to:", argsFile);
 
-      console.log(`
+        const contractFullName = `${artifact.sourceName}:${artifact.contractName}`;
+
+        console.log(`
         Verify using:
 
             hh verify ${contract.address} --constructor-args ${argsFile} --network ${hre.network.name} --contract ${contractFullName}
       `);
 
-      const txCost = receipt.gasUsed.mul((tx as any).maxFeePerGas);
+        await contract.deployed();
 
-      console.log(
-        `\n| --------------------- | ----------------------------- |`,
-        `\n| gasUsed \t\t| ${receipt.gasUsed.toString()}\t\t\t|`,
-        `\n| gasPrice\t\t| ${tx.gasPrice?.toString()} gwei\t\t|`,
-        `\n| totalCost\t\t| ${formatEther(txCost)} ETH\t|`,
+        const tx = contract.deployTransaction;
 
-        `\n| --------------------- | ----------------------------- |`,
-        `\n| contractAddress\t| ${contract.address}\t|`,
-        `\n| --------------------- | ----------------------------- |`
-      );
+        const receipt = await tx.wait();
 
-      const verificationId = await hre.run("verify:verify", {
-        address: contract.address,
-        contract: `${artifact.sourceName}:${artifact.contractName}`,
-        constructorArguments: args,
-      });
+        const txCost = receipt.gasUsed.mul((tx as any).maxFeePerGas);
 
-      console.log(
-        "Check the verification status: \n\n\thh verify-status --verification-id",
-        verificationId,
-        "\n"
-      );
-    } catch (err: any) {
-      console.error(
-        err?.error?.error ??
-          err?.error?.message ??
-          err?.error ??
-          err?.message ??
-          err
-      );
+        console.log(
+          `\n| --------------------- | ----------------------------- |`,
+          `\n| gasUsed \t\t| ${receipt.gasUsed.toString()}\t\t\t|`,
+          `\n| gasPrice\t\t| ${tx.gasPrice?.toString()} gwei\t\t|`,
+          `\n| totalCost\t\t| ${formatEther(txCost)} ETH\t|`,
 
-      console.log("Failed to deploy the contract.");
+          `\n| --------------------- | ----------------------------- |`,
+          `\n| contractAddress\t| ${contract.address}\t|`,
+          `\n| --------------------- | ----------------------------- |`
+        );
 
-      process.exit(1);
+        const verificationId = await hre.run("verify:verify", {
+          address: contract.address,
+          contract: `${artifact.sourceName}:${artifact.contractName}`,
+          constructorArguments: args,
+        });
+
+        console.log(
+          "Check the verification status: \n\n\thh verify-status --verification-id",
+          verificationId,
+          "\n"
+        );
+      } catch (err: any) {
+        console.error(
+          err?.error?.error ??
+            err?.error?.message ??
+            err?.error ??
+            err?.message ??
+            err
+        );
+
+        console.log("Failed to deploy the contract.");
+
+        process.exit(1);
+      }
     }
-  });
+  );
